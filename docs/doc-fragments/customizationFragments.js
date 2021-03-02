fe6f0a572d6d7392c30f2399ec51c3b5ab7ae0dd
minhocha <minhocha1004@gmail.com>
2021-03-02 11:15:42 -0800

    push

--- docs/doc-fragments/customizationFragments.js
+++
@@ -1,5 +0,0 @@
-var premadeFlavors = require('./premadeFlavors');
-var buildYourOwn = require('./buildYourOwnFlavor');
-var flavorTools = require('./flavorTools');
-
-module.exports = [premadeFlavors, flavorTools, buildYourOwn]
--- docs/vinf.js
+++
@@ -1,4 +0,0 @@
-// Get the current version of the Gluon branch.
-module.exports = {
-  version: 'v3.0.1'
-}
